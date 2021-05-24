60512fe11cd212a1f805688b9dbaeb6cf038cdd5
Bart Lannoeye <bart.lannoeye@gmail.com>
2021-05-24 19:44:49 +0200

    Sample code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-2021-05-WebApps-TimeZone
+# Use a specific timezone in Azure App Service.
+Azure services, including App Service, run in UTC by default. If your software uses local time for any reason (storing dates, parsing csv files, ...) then you'll curse after migrating your workload to the cloud. There's the option to start messing around with timezones in code to keep everything working, but you're also able to explicitly tell Azure App Service to operate in a specific time zone.
+
+Clone this repository to have a quick deployable project to test if your Azure App Service is handling time as desired.
+
+Blog post: https://bartlannoeye.com/blog/timezone-for-azure-appservice/
---
+++ src/Timezone/Controllers/TimeZoneController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Text;
+
+namespace Timezone.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TimeZoneController : ControllerBase
+    {
+        private readonly ILogger<TimeZoneController> _logger;
+
+        public TimeZoneController(ILogger<TimeZoneController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string GetTime()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            // Get the local time zone and the current local time and year.
+            var localZone = TimeZoneInfo.Local;
+            DateTime currentDate = DateTime.Now;
+
+            // Display the names for standard time and daylight saving 
+            // time for the local time zone.
+            builder.AppendLine($"Standard time name: {localZone.StandardName}");
+            builder.AppendLine($"Daylight saving time name: {localZone.DaylightName}");
+
+            // Display the current date and time and show if they occur 
+            // in daylight saving time.
+            builder.AppendLine($"Current date and time: {currentDate:F}");
+            builder.AppendLine($"Daylight saving time? {localZone.IsDaylightSavingTime(currentDate)}");
+
+            // Get the current Coordinated Universal Time (UTC) and UTC 
+            // offset.
+            builder.AppendLine($"Coordinated Universal Time: {currentDate.ToUniversalTime():F}");
+            builder.AppendLine($"UTC offset: {localZone.GetUtcOffset(currentDate)}");
+
+            return builder.ToString();
+        }
+    }
+}
